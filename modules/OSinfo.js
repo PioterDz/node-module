edddb358a59c8eeecb21ccd072be214813ea1c45
Piotr Dzienis <dojlidy.zubr@gmail.com>
2019-04-03 11:31:03 +0200

    make time format function with small bug

--- modules/OSinfo.js
+++ modules/OSinfo.js
@@ -11,12 +11,11 @@ function getOSinfo() {
     var release = os.release();
     var cpu = os.cpus()[0].model;
     var uptime = os.uptime();
-    changeTime.formatedTime(uptime);
     var userInfo = os.userInfo();
     console.log('System:', type);
     console.log('Release:', release);
     console.log('CPU model:', cpu);
-    console.log('Uptime: ~', time);
+    console.log('Uptime: ~', changeTime.time(uptime));
     console.log('User name:', userInfo.username);
     console.log('Home dir:', userInfo.homedir);
 }
