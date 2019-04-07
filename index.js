2a8d0815ba9c585364bce470e8d7c7d1436a0c18
Piotr Dzienis <dojlidy.zubr@gmail.com>
2019-04-07 18:50:28 +0200

    make readdir and writeFile functions

--- index.js
+++ index.js
@@ -1,39 +1,12 @@
-var event = require('events').EventEmitter;
-var OSinfo = require('./modules/OSInfo');
+var fs = require('fs');
+var colors = require('colors');
 
-var emitter = new EventEmitter();
-emitter.on('beforeCommand', function(instruction) {
-    console.log('You wrote: ' + instruction + ' trying to run command.')
+
+fs.readdir('./', function(err, data) {
+    if (err) throw err;
+    console.log('data: ' + data);
+    fs.writeFile('message.txt', data, function(err) {
+        if (err) throw err;
+        console.log('Zapisano!'.blue);
+    });
 });
-emitter.on('afterCommand', function() {
-console.log('Finished command');
-});
-
-process.stdin.setEncoding('utf-8');
-
-
-
-process.stdin.on('readable', function() {
-    var input = process.stdin.read();
-    if(input !== null) {
-        var instruction = input.trim();
-        emitter.on('beforeCommand', instruction);
-        switch(instruction) {
-            case '/exit':
-                process.stdout.write('Quitting app!\n');
-                process.exit();
-                break;
-            case '/sayhello':
-                process.stdout.write('hello!\n');
-                break;
-            case '/getOSinfo':
-                OSinfo.print();
-                break;
-            default:
-                process.stderr.write('Wrong instruction!\n');
-        };
-        emitter.emit('afterCommand');
-    }
-});
-
-
