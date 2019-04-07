aa3496420341ea7a22df6800ea0dcbbba677add7
Piotr Dzienis <dojlidy.zubr@gmail.com>
2019-04-07 20:37:28 +0200

    first commit

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
-var fs = require('fs');
-var colors = require('colors');
+var http = require('http');
 
+var server = http.createServer();
 
-fs.readdir('./', function(err, data) {
-    if (err) throw err;
-    console.log('data: ' + data);
-    fs.writeFile('message.txt', data, function(err) {
-        if (err) throw err;
-        console.log('Zapisano!'.blue);
-    });
+server.on('request', function (request, response) {
+    response.setHeader('Content-Type', 'text/html; charset=utf-8');
+    if(request.method === 'GET' && request.url === '/hello') {
+        response.write('<h1>Hello World!</h1>');
+            response.end();
+    }
+    else {
+        response.statusCode = 404;
+        response.write('<h1>404: Zła ścieżka!</h1>');
+        response.end();
+    }
 });
+
+server.listen(8080);
