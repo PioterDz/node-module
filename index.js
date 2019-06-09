a0ab98336828a30d9b7d08d179e93e1756b99111
Piotr Dzienis <dojlidy.zubr@gmail.com>
2019-06-09 20:09:14 +0200

    put json.stringify

--- index.js
+++ index.js
@@ -5,12 +5,9 @@ var os = require('os');
 
 fs.readdir('./', function(err, data) {
     if (err) throw err;
-    var dataJSON = JSON.stringify(data);
-    console.log('data: ' + dataJSON);
-    for(var i = 0 ; i <dataJSON.length ; i++) {
-        return dataJSON[i] + '\n';
-    }
-    console.log('data: ' + dataJSON);
+    // var dataJSON = JSON.stringify(data);
+    // console.log('data: ' + dataJSON);
+    console.log('data: ' + data);
     fs.writeFile('message.txt', data, function(err) {  
         if (err) throw err;
         console.log('Zapisano!'.blue);
