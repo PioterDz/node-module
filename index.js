9272d699eab0e08a2b619b6c996a38f04a7fadb7
Piotr Dzienis <dojlidy.zubr@gmail.com>
2019-06-09 20:08:39 +0200

    delete modules folder

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 var fs = require('fs');
 var colors = require('colors');
+var os = require('os');
 
 
 fs.readdir('./', function(err, data) {
     if (err) throw err;
-    console.log('data: ' + data);
-    fs.writeFile('message.txt', data, function(err) {
+    var dataJSON = JSON.stringify(data);
+    console.log('data: ' + dataJSON);
+    for(var i = 0 ; i <dataJSON.length ; i++) {
+        return dataJSON[i] + '\n';
+    }
+    console.log('data: ' + dataJSON);
+    fs.writeFile('message.txt', data, function(err) {  
         if (err) throw err;
         console.log('Zapisano!'.blue);
     });
