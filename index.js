0be4d3c046ef13dce49e4f31b7f6353c8a17429e
Piotr Dzienis <dojlidy.zubr@gmail.com>
2019-06-09 23:20:40 +0200

    make forEach to append file names in new line

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 var fs = require('fs');
 var colors = require('colors');
-var os = require('os');
-
 
 fs.readdir('./', function(err, data) {
     if (err) throw err;
-    // var dataJSON = JSON.stringify(data);
-    // console.log('data: ' + dataJSON);
-    console.log('data: ' + data);
-    fs.writeFile('message.txt', data, function(err) {  
-        if (err) throw err;
-        console.log('Zapisano!'.blue);
+    data.forEach(item => {
+        fs.appendFile('message.txt', `${item} \r\n`, (err) => {  
+            if (err) throw err;
+            console.log('Zapisano!'.blue);
+        });
     });
 });
