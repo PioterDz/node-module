66aaf969fdf032b9b7f5a1f05b3fc5c582f6d5b8
Piotr Dzienis <dojlidy.zubr@gmail.com>
2019-06-10 14:17:56 +0200

    make fs.readFile function to response with index.html

--- index.js
+++ index.js
@@ -1,26 +1,32 @@
 var http = require('http');
 var fs = require('fs');
-
+var port = 8000;
 var server = http.createServer();
 
 server.on('request', function (request, response) {
     response.setHeader('Content-Type', 'text/html; charset=utf-8');
-    if(request.method === 'GET' && request.url === '/hello') {
-        response.write('<h1>Hello World!</h1>');
-            response.end();
+    // response.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+    if(request.method === 'GET' && request.url === '/') {
+        fs.readFile('./index.html', 'utf-8', function(err, data) {
+            if (err) {
+                throw err;
+            }
+            response.end(data);
+        });
     }
     else {
         response.statusCode = 404;
-        response.write('<img src="https://linube.com/blog/wp-content/uploads/error-404.jpg"></img>');
+        response.write('<img src="https://www.interserver.net/tips/wp-content/uploads/2016/10/404error.jpeg"></img>');
         response.end();
+        // fs.readFile('./404error.jpeg', function(err, data) {
+        //     if (err) {
+        //         throw err;
+        //     }
+        //     response.write(data);
+        //     response.end();
+        // })
     }
 });
 
-server.listen(8080);
-
-fs.readFile('./index.html', 'utf-8', function(err, data) {
-    if (err) {
-        throw err;
-    }
-    console.log(data);
-});
+server.listen(port);
+console.log('listening');
